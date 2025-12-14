04dd639de5adf08ec5489f631b51a18151f50730
mikmin2 <dinardochristopher0@gmail.com>
2025-12-14 17:40:46 -0500

    Completed the Address Class with all it's fields and methods

--- src/main/java/org/mikmin/Address.java
+++ src/main/java/org/mikmin/Address.java
@@ -1,12 +1,61 @@
 package org.mikmin;
 
-public class Address {
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@ToString
+@EqualsAndHashCode
+@Getter
+@Setter
+public class Address {
     private int streetNo;
     private String street;
     private String city;
     private Province province;
     private String postalCode;
+
+
+    public Address(int streetNo, String street, String city, Province province, String postalCode) {
+        if (isPostalCodeValid(postalCode)) {
+            this.streetNo = streetNo;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.postalCode = postalCode;
+        }
+
+        this.streetNo = 0;
+        this.street = null;
+        this.city = null;
+        this.province = null;
+        this.postalCode = null;
+    }
+
+    /**
+     * Checks if a given postal code is valid
+     * @param postalCode input postal code
+     * @return the validity of the postal code
+     */
+    public static boolean isPostalCodeValid(String postalCode) {
+        if (postalCode != null && postalCode.length() == 6) {
+            char char1 = postalCode.charAt(0);
+            char char2 = postalCode.charAt(1);
+            char char3 = postalCode.charAt(2);
+            char char4 = postalCode.charAt(3);
+            char char5 = postalCode.charAt(4);
+            char char6 = postalCode.charAt(5);
+
+            return (Character.isLetter(char1)
+                    && Character.isLetter(char3)
+                    && Character.isLetter(char5)
+                    && Character.isDigit(char2)
+                    && Character.isDigit(char4)
+                    && Character.isDigit(char6));
+        }
+        return false;
+    }
 
 
 
